3712a6d9dda0613ee2518af79122ea25ef3e92a7
DR4GON\DeLong <gaming@delong-iu.de>
2023-09-30 11:50:30 +0200

    Add visual spacers for better overview

--- CodeWars/DeLongsSolution.java
+++ CodeWars/DeLongsSolution.java
@@ -16,16 +16,20 @@ public class DeLongsSolution {
       ++testCounter;
       System.out.println("Test number:" + testCounter);
       System.out.println("With field count: " + chessBoard.length);
+      System.out.println("------------------------------------");
 
       HashMap<Integer, Integer> countEmptyFields = new HashMap<Integer, Integer>();
 
       for (int i = 0; i < chessBoard.length; i++) {
          for (int j = 0; j < chessBoard[i].length; j++) {
+            System.out.println("Field: " + i + " " + j + " Value: " + chessBoard[i][j]);
             if (chessBoard[i][j] == 1) {
                if (countEmptyFields.containsKey(i)) {
+                  System.out.println("Adding to count in field: " + i);
                   // For each field with 1 the count is added up
                   countEmptyFields.put(i, countEmptyFields.get(i) + 1);
                } else {
+                  System.out.println("Init count in field: " + i);
                   // Init for the field count
                   countEmptyFields.put(i, 1);
                }
@@ -39,6 +43,8 @@ public class DeLongsSolution {
             .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
             .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey, (e1, e2) -> e1, HashMap::new));
 
+      System.out.println("------------------------------------");
+      System.out.println("");
       return countEmptyFieldsReversed;
    }
 }
