f3f7ce13256c6121e9cffa97686f4d8245f7763c
DR4GON\DeLong <gaming@delong-iu.de>
2023-09-30 10:46:37 +0200

    Add CodeWars reference link

--- CodeWars/Main.java
+++ CodeWars/Main.java
@@ -1,3 +1,6 @@
+/**
+ * Solving this https://www.codewars.com/kata/5bc6f9110ca59325c1000254/java
+ */
 public class Main {
    public static void main(String[] args) {
       System.out.println("Hello World");
